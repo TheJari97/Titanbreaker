a719db3a773fa976781f6ffc844d52f9114a91aa
Jari <JuanAriza.@hotmail.com>
2026-04-21 10:53:37 -0500

    Eliminacion

--- assets/js/core/state.js
+++
@@ -1,2 +0,0 @@
-
-window.AppState = { selectedRarities: new Set(AppUtils.rarityOrder), itemSearch:'', craftSearch:'', itemsFiltersOpen:false, talentLevels:{}, bonusPoints:0, selectedHero:null, heroTalentChoices:{} };
